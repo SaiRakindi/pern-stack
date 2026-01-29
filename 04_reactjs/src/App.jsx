1c8d57ca80ffca4378625bc28635129f30a334e5
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-29 13:19:57 +0530

    feat: react setup

---
+++ 04_reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Car from "./components/Car";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Car</h1>
+
+      <ul>
+        <Car />
+      </ul>
+    </div>
+  );
+};
+
+export default App;
